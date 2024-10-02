85955340673aed339a40d680b6679b9c3b24f858
MMMohebi <MMMohebi@outlook.com>
2024-10-02 14:31:08 +0330

    fix removeNonCommentLines

--- utils.go
+++ utils.go
@@ -3,88 +3,31 @@ package iran_resolver
 import (
 	"bufio"
 	"fmt"
-	"github.com/pkg/errors"
-	"io"
 	"os"
 	"strings"
 )
 
 func removeNonCommentLines(filePath string) error {
-	file, err := os.Open(filePath)
+	allLines, err := readLines(filePath)
 	if err != nil {
-		return fmt.Errorf("error opening file: %w", err)
-	}
-	defer file.Close()
-
-	var validLines []string
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text()) // Trim whitespace
-
-		if line == "" || strings.HasPrefix(line, "#") {
-			validLines = append(validLines, line)
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
 		return fmt.Errorf("error reading file: %w", err)
 	}
 
-	tempFile, err := os.CreateTemp("", "tempfile-")
-	if err != nil {
-		return fmt.Errorf("error creating temp file: %w", err)
-	}
-	defer os.Remove(tempFile.Name()) // Clean up the temporary file
+	var validLines []string
 
-	for _, line := range validLines {
-		_, err := tempFile.WriteString(line + "\n")
-		if err != nil {
-			return fmt.Errorf("error writing to temp file: %w", err)
+	for _, line := range allLines {
+		l := strings.TrimSpace(line) // Trim whitespace
+
+		if l == "" || strings.HasPrefix(l, "#") {
+			validLines = append(validLines, l)
 		}
 	}
 
-	if err := tempFile.Close(); err != nil {
-		return err
+	err = writeLines(validLines, filePath)
+	if err != nil {
+		return fmt.Errorf("error writing file: %w", err)
 	}
 
-	if err != nil && strings.Contains(err.Error(), "invalid cross-device link") {
-		return moveCrossDevice(tempFile.Name(), filePath)
-	}
-	if err != nil {
-		return fmt.Errorf("error renaming temp file to original file: %w", err)
-	}
-
-	return nil
-}
-
-func moveCrossDevice(source, destination string) error {
-	src, err := os.Open(source)
-	if err != nil {
-		return errors.Wrap(err, "Open(source)")
-	}
-	dst, err := os.Create(destination)
-	if err != nil {
-		src.Close()
-		return errors.Wrap(err, "Create(destination)")
-	}
-	_, err = io.Copy(dst, src)
-	src.Close()
-	dst.Close()
-	if err != nil {
-		return errors.Wrap(err, "Copy")
-	}
-	fi, err := os.Stat(source)
-	if err != nil {
-		os.Remove(destination)
-		return errors.Wrap(err, "Stat")
-	}
-	err = os.Chmod(destination, fi.Mode())
-	if err != nil {
-		os.Remove(destination)
-		return errors.Wrap(err, "Stat")
-	}
-	os.Remove(source)
 	return nil
 }
 
@@ -98,4 +41,33 @@ func removeDuplicates(s []string) []string {
 		}
 	}
 	return result
+}
+
+func writeLines(lines []string, path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+	return w.Flush()
+}
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
